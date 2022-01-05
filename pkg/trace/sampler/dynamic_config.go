1fcaffb270042aaacf80f25d68822b434a436d16
Barbayar DASHZEVEG <barbayar.dashzeveg@datadoghq.com>
2022-01-05 15:19:51 +0100

    a new version generator for DynamicConfig

--- pkg/trace/sampler/dynamic_config.go
+++ pkg/trace/sampler/dynamic_config.go
@@ -76,7 +76,7 @@ func (rbs *RateByService) SetAll(rates map[ServiceSignature]rm) {
 	}
 	if changed {
 		rbs.rates, rbs.incomingRates = rbs.incomingRates, rbs.rates
-		rbs.version = strconv.FormatInt(time.Now().UnixNano(), 16)
+		rbs.version = newVersion()
 	}
 }
 
@@ -103,4 +103,11 @@ func (rbs *RateByService) GetNewState(version string) State {
 	}
 
 	return ret
+}
+
+var localVersion int64 = 0
+
+func newVersion() string {
+	localVersion++
+	return strconv.FormatInt(time.Now().Unix(), 16) + "-" + strconv.FormatInt(localVersion, 16)
 }
